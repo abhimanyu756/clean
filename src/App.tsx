6d89223ac5ee582094af000bde500e3270bc5b11
abhimanyu756 <abhimanyu81026@gmail.com>
2025-11-20 18:20:24 +0530

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Home from "./home";
 // import "./App.css"; <-- This line will be removed
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Home />
